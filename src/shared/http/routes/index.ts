2371d81fb715170435e8fc48a40ac08b40d6dbe2
jlribeirojr <jlribeirojr@gmail.com>
2024-03-24 12:09:08 -0300

    add roles and routes e uuid

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import { rolesRouter } from '@roles/http/routes/roles.routes'
 
 const routes = Router()
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Ola Dev!' })
 })
+
+routes.use('/roles', rolesRouter)
 
 export { routes }
