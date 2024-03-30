9cb6d98387c9d3eb4492bb8d1e1b0f724570e1df
jlribeirojr <jlribeirojr@gmail.com>
2024-03-29 21:36:24 -0300

    Add data validation

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import swaggerUi from 'swagger-ui-express'
 import cors from 'cors'
+import { errors } from 'celebrate'
 import { routes } from './routes'
 import { AppError } from '@shared/errors/AppError'
 import swaggerFile from '../../swagger.json'
@@ -13,6 +14,9 @@ app.use(express.json())
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(routes)
+
+app.use(errors())
+
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
