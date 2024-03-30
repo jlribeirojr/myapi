7b04eda3ec1f2ea0559c48edd4e407b2472b41ed
jlribeirojr <jlribeirojr@gmail.com>
2024-03-30 18:30:42 -0300

    create table addRoleIdToUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,16 @@
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1711585825551 } from './migrations/1711585825551-CreateRolesTable'
 import { Role } from '@roles/entities/Roles'
+import { CreateUsersTable1711833230356 } from './migrations/1711833230356-CreateUsersTable'
+import { AddRoleIdToUsersTable1711833809316 } from './migrations/1711833809316-AddRoleIdToUsersTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1711585825551],
+  migrations: [
+    CreateRolesTable1711585825551,
+    CreateUsersTable1711833230356,
+    AddRoleIdToUsersTable1711833809316,
+  ],
 })
