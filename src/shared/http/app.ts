1df9b83cf98df486903d5e02ac398ed13e228eaa
jlribeirojr <jlribeirojr@gmail.com>
2024-03-31 22:58:11 -0300

    add userAvatarUrl Propety

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -2,22 +2,21 @@ import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import swaggerUi from 'swagger-ui-express'
 import cors from 'cors'
+import path from 'node:path'
 import { errors } from 'celebrate'
 import { routes } from './routes'
 import { AppError } from '@shared/errors/AppError'
 import swaggerFile from '../../swagger.json'
 import '@shared/container'
+import uploadConfig from '@config/upload'
 
 const app = express()
 app.use(cors())
-
 app.use(express.json())
+app.use('/files', express.static(uploadConfig.directory))
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-
 app.use(routes)
-
 app.use(errors())
-
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
